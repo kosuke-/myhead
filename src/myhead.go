a99d1ee0afb3535334ff68b0d678311d6fdbebd7
Kosuke Niwano <kosuke.niwano@groove-x.com>
2018-04-07 17:19:34 +0900

    add

--- src/myhead.go
+++ src/myhead.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"bufio"
+	"fmt"
+	"strings"
 )
 
 type Myhead struct {
@@ -20,6 +21,13 @@ func NewMyhead(number int, filepath []string) *Myhead{
 }
 
 func (m *Myhead) Run()  {
+
+	if(0 >= m.number){
+		return
+	}
+
+	textMap := make(map[string]string)
+
 	for _,v := range m.filepath {
 		fp, err := os.Open(v)
 		if err != nil {
@@ -28,17 +36,35 @@ func (m *Myhead) Run()  {
 
 		scanner := bufio.NewScanner(fp)
 		count := 0
+		buf := ""
 		for scanner.Scan() {
 			if (count >= m.number) {
 				break;
 			}
-			fmt.Println(scanner.Text())
+			buf += scanner.Text()
+			buf += "\n";
 			count++
 		}
-		if err := scanner.Err(); err != nil {
-			return;
-		}
+		buf = strings.TrimRight(buf, "\n")
+		textMap[v] = buf
 		fp.Close()
+	}
+
+	headPrint(textMap)
+	return
+}
+
+func headPrint(data map[string]string){
+
+	size := len(data)
+	count := 0
+	for k,v := range data{
+		count ++
+		fmt.Printf("==> %s <==\n",k)
+		fmt.Println(v)
+		if size != count{
+			fmt.Println()
+		}
 	}
 	return
 }
